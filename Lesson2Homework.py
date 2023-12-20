797b3323c564c131df0cb3b08cc4f80fd12c5047
Nathan <nathandyzh@gmail.com>
2023-12-20 13:19:38 -0500

    Updated Lesson 2 Homework
    With teacher questions and my questions

--- Lesson2Homework.py
+++ Lesson2Homework.py
@@ -1,3 +1,13 @@
+# Exercise 3: Finding the Largest Value
+# 1. Create a list of the following numbers: 100, 50, 75, 90, 55. Call the list numbers.
+# 2. Create a function called get_largest_value(numbers), with parameter numbers.
+#         a. Inside the function, find the largest number in numbers and store the value in a variable called largest. (Hint, use a 
+#            for loop with an if statement inside the for loop. Compare the current number in the list to the current value of 
+#            largest, and update largest if needed).
+#        b. Return largest.
+# 3. Call the function, passing numbers as an input to the function. Store the return value of the function in a variable 
+#         called largest.
+# 4. Print the largest.
 numbers = [100, 50, 75, 90, 55]
 def get_largest_value(numbers):
     largest = numbers[0]
@@ -6,5 +16,7 @@ def get_largest_value(numbers):
             largest = n
     return largest
 
-largest = get_largest_value(numbers)
-print( f"{largest} is the largest number in the list")
+largest_num = get_largest_value(numbers)
+print( f"{largest_num} is the largest number in the list")
+#How to use debug in VS Code
+# Parameters or Arguments?
