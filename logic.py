3184408bc04c416169e6287c588dc9bb227f91bf
Nathan <nathandyzh@gmail.com>
2024-04-16 08:17:10 -0400

    logic.py

---
+++ logic.py
@@ -0,0 +1,20 @@
+def problem1(n):
+    if n >= 0:
+        return True
+    else:
+        return False
+def problem2(n):
+    if n < 1 or n > 5:
+        return True
+    else:
+        return False
+def problem3(n):
+    if n > 3 > 5 > 15:
+        return True
+    else:
+        return False
+def problem4(n):
+    if (1 <= n <= 5) or (12 <= n <= 20):
+        return True
+    else:
+        return False
