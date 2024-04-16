22d6e18ff2787690421dde28000198e9d744e4a5
Nathan <nathandyzh@gmail.com>
2024-04-16 08:16:11 -0400

    4-12-2024.py

---
+++ 4-12-2024.py
@@ -0,0 +1,26 @@
+def isValid(n):
+    valid = False
+    if(n == 1):
+        valid = True
+    elif(n == 2):
+        valid = True
+    elif(n == 3):
+        valid = True
+    elif(n == 4):
+        valid = True
+    elif(n == 5):
+        valid = True
+    else:
+        valid = False
+    return valid
+
+a = False
+b = True
+c = False
+d = False
+e = True
+f = True
+
+x = ((a or b) and (f and not(c))) or ((d or c) or (e or not(f)))
+
+n = input("Please enter a number 1-5: ")
