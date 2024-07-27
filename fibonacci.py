8f8330ca501f408ee272a2f70a4a4163559c7a08
Nathan <nathandyzh@gmail.com>
2024-07-27 10:15:24 -0400

    Class on 7/26/24

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fib(n, table = dict()):
+    if(n <= 1):
+        return 1
+    if(n in table):
+        return table[n]
+    else:
+        table[n] = fib(n - 1) + fib(n - 2)
+        return table[n]
+
+print("fib:", fib(50))
