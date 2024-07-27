ea597ee4975f780ad97e3c3b545e96b58aaed011
Nathan <nathandyzh@gmail.com>
2024-07-27 10:15:37 -0400

    Homeworks

---
+++ recursive.py
@@ -0,0 +1,13 @@
+def S(n, memo={1: 5, 2: 6, 3: 7}):
+    if n in memo:
+        return memo[n]
+    memo[n] = S(n-1, memo) * S(n-2, memo) * S(n-3, memo)
+    return memo[n]
+
+print(S(1))  
+print(S(2))  
+print(S(3))  
+print(S(4)) 
+print(S(5))  
+print(S(6))  
+print(S(7))  
