eb80a3c178a6110658107e21a851e50fa0d684a2
Nathan <nathandyzh@gmail.com>
2024-10-13 10:27:53 -0400

    Update classes.cpp
    Added getters and setters

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -7,59 +7,109 @@ using namespace std;
 class Dog
 {
     private:
+        string name;
         string breed;
         string color;
         int age;
         float weight;
     public:
-        Dog(string b, string c, int a, float w)
+        Dog(string n, string b, string c, int a, float w)
         {
-            this -> setBreed(b);
-            this -> setColor(c);
-            this -> setAge(a);
-            this -> setWeight(w);
+            setName(n);
+            setBreed(b);
+            setColor(c);
+            setAge(a);
+            setWeight(w);
         }
 
+        void setName(string n)
+        {
+            name = n;
+        }
         void setBreed(string b)
         {
-            this -> breed = b;
+            breed = b;
         }
         void setColor(string c)
         {
-            this -> color = c;
+            color = c;
         }
         void setAge(int a)
         {
-            this -> age = a;
+            age = a;
         }
         void setWeight(float w)
         {
-            this -> weight = w;
+            weight = w;
         }
 
+        string getName()
+        {
+            return name;
+        } 
         string getBreed()
         {
-            return this -> breed;
+            return breed;
         }
         string getColor()
         {
-            return this -> color;
+            return color;
         }
         int getAge()
         {
-            return this -> age;
+            return age;
         }
         float getWeight()
         {
-            return this -> weight;
+            return weight;
         }
 };
 
+Dog dogFunction(Dog dog1, Dog dog2)
+{
+    if(dog1.getWeight() >= dog2.getWeight())
+    {
+        return dog1;
+    }
+    else
+    {
+        return dog2;
+    }
+}
 
+
+string function(int x)
+{
+    return "hello";
+}
+string function(string x)
+{
+    return "world";
+}
+string function(int x, string y)
+{
+    return "hi";
+}
 
 
 int main()
 {
-    Dog dog1("poodle", "black", 6, 45.2);
+    Dog dog1("Fido", "poodle", "black", 6, 45.2);
+    Dog dog2("Spot", "golden retriever", "yellow", 3, 55.2);
+
+// Dog dog3();
+
+    int x = 5;
+    cout << function(x) << endl;
+
+    string y = "abc";
+    cout << function(y) << endl;
+
+    cout << function(x, y) << endl;
+
+
+    Dog heavyDog = dogFunction(dog1, dog2);
+    cout << heavyDog.getName() << endl;
+    
     return 0;
 }
