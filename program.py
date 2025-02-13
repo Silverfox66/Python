c6e6ca191003fa1dbd44bfab36652066babc678e
Nathan <nathandyzh@gmail.com>
2025-02-13 17:44:53 -0500

    program.py
    Added instructions

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+#Defines a function that takes a list of names and a letter as input, then prints the names from the list that start with the given letter.
 def print_names_by_letter(names, letter):
     for name in names:
         if(name[0] == letter):
