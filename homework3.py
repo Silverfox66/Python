8200478836c5013319e83fbb186b849fa2f38397
Nathan <nathandyzh@gmail.com>
2025-02-13 17:44:01 -0500

    homework3.py
    Added instructions

--- homework3.py
+++ homework3.py
@@ -1,3 +1,5 @@
+#Calculate and print the sum of all the natural numbers below 1000 that are multiples of 3 or 5.
+
 sum_of_multiples = 0
 for num in range(1000):
     if num % 3 == 0 or num % 5 == 0:
