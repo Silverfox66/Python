b1fa574c7d438d64b4adabdb00af2f69e0f68efd
Nathan <nathandyzh@gmail.com>
2025-02-13 17:45:36 -0500

    switchInteger.py
    Added instructions

--- switchInteger.py
+++ switchInteger.py
@@ -1,3 +1,5 @@
+# Demonstrate two methods for swapping the values of two variables: one using arithmetic and the other using the XOR operator.
+
 def swap_numbers_arithmetic(a, b):
     print(f"Original a: {a}, b: {b}")
     a = a + b
